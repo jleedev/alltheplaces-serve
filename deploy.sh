845d0215579f7e0eabf3185f297ac825d136bc5b
Josh Lee <jleedev@gmail.com>
2022-01-10 16:16:49 +0000

    adjust build

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@
 set -eux
 
 PROJECT=$(gcloud config get-value project)
-gcloud builds submit --tag gcr.io/$PROJECT/alltheplaces
-gcloud run deploy --platform managed alltheplaces --image gcr.io/$PROJECT/alltheplaces --memory 1024Mi
+gcloud builds submit --tag gcr.io/$PROJECT/alltheplaces --timeout 30m
+gcloud run deploy --platform managed alltheplaces --image gcr.io/$PROJECT/alltheplaces --memory 128Mi
 
 gcloud container images list-tags gcr.io/$PROJECT/alltheplaces --format=json | jq -rc '.[]|select(.tags==[]).digest' | while read DIGEST
 do
