51fdb89628ab2c045e64db17d599b3c67050300e
Josh Lee <jleedev@gmail.com>
2022-01-13 15:39:13 +0000

    Adjust project

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,36 @@
 
 set -eux
 
-PROJECT=$(gcloud config get-value project)
-gcloud builds submit --tag gcr.io/$PROJECT/alltheplaces --timeout 30m
-gcloud run deploy --platform managed alltheplaces --image gcr.io/$PROJECT/alltheplaces --memory 128Mi
+[ -r .env ] && . .env
 
-gcloud container images list-tags gcr.io/$PROJECT/alltheplaces --format=json | jq -rc '.[]|select(.tags==[]).digest' | while read DIGEST
+[ -v PROJECT_ID ] || (
+  PROJECT_ID="$(gcloud config get-value project)"
+  [ "$PROJECT_ID" != "" ]
+)
+
+gcloud() {
+  command gcloud --project=$PROJECT_ID "$@"
+}
+
+# Asserts that the APIs are enabled
+gcloud builds list
+gcloud run services list
+
+gcloud builds submit \
+  --tag gcr.io/$PROJECT_ID/alltheplaces --timeout 30m
+
+gcloud run deploy \
+  --platform managed alltheplaces \
+  --image gcr.io/$PROJECT_ID/alltheplaces \
+  --memory 128Mi
+
+gcloud container images list-tags \
+  gcr.io/$PROJECT_ID/alltheplaces --format=json \
+  | jq -rc '.[]|select(.tags==[]).digest' \
+  | while read DIGEST
 do
-  gcloud container images delete gcr.io/$PROJECT/alltheplaces@$DIGEST
+  gcloud container images delete \
+    gcr.io/$PROJECT_ID/alltheplaces@$DIGEST
 done
 
 # vi: ts=2 sw=2 et
